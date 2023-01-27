c08f7b0a7204c041be38720eb8fd91026a9a8727
Popular234 <cainjose24@g.crookcountyschools.org>
2023-01-27 14:21:05 -0800

    yup

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+// console.log("hello world");
+
+document.write('<h1> Array Demo </h1>')
+
+// const drinks =['Coffee', 'Water', 'Soder', 'Tea']
+
+// drinks.push('juice')
+
+// for(let i=0; i<5; i++){
+//     console.log(drinks[i]);
+// }
+
+// drinks.push('juice')
+// this ^ does not work it needs to be above
+// console.log(drinks)
+
+const hours = ['9am ', '10am ', '1pm ', '2pm ', '3pm ', '4pm'];
+hours.unshift('open');
+let start = 3;
